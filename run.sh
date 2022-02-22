c8f5cb22b643b9575d4e08f35f29b7d63012afd5
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-02-22 13:14:06 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-echo "compile main.cpp"
-g++ main.cpp gol.hpp gol.cpp -o main.exe && echo "run main.exe"
-echo "--------------------------------"
-./main.exe
-echo "--------------------------------"
-echo "remove main.exe"
-rm main.exe
+cmake -S . -B build && \
+cmake --build build && \
+build/gol
