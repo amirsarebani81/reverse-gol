5e64780429f0ed0f3463e2ef964589b68a64ec2a
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-03-02 12:49:54 +0330

    Refactor gol.hpp for better abtraction

--- gol.hpp
+++ gol.hpp
@@ -14,8 +14,8 @@ private:
     std::size_t level;
 
 public:
-    Gol(const Table<char> &t, const std::size_t l)
-        : final(t), level(l) {}
+    Gol(const Table<char> &table, const std::size_t lev)
+        : final(table), level(lev) {}
 
     Table<char> solve() const
     {
@@ -23,85 +23,72 @@ public:
         const size_t bound = static_cast<std::size_t>(1) << final.get_height() * final.get_width();
         for (size_t k = 0; k < bound; k++)
         {
-            const Table<char> updated_table = update(t, level);
-            if (final == updated_table)
+            if (final == update(t, level))
                 return t;
             next(t);
         }
         return Table<char>();
     }
 
-    void next(Table<char> &t) const
+    void next(Table<char> &table) const
     {
-        if (t[0][0] == EMPTY)
+        if (table[0][0] == EMPTY)
         {
-            t[0][0] = MUSHROOM;
+            table[0][0] = MUSHROOM;
             return;
         }
-        for (std::size_t i = 0; i < t.get_height(); i++)
-        {
-            for (std::size_t j = 0; j < t.get_width(); j++)
-            {
-                if (t[i][j] == MUSHROOM)
-                {
-                    t[i][j] = EMPTY;
-                    continue;
-                }
-                t[i][j] = MUSHROOM;
-                return;
-            }
-        }
-    }
-
-    Table<char> update(const Table<char> &t, const std::size_t level) const
-    {
-        Table<char> current = t, prev = t;
-        for (std::size_t i = 0; i < level; i++)
-        {
-            prev = current;
-            const Table<int> adj = adjacents(prev);
-
-            for (std::size_t i = 0; i < adj.get_height(); i++)
-            {
-                for (std::size_t j = 0; j < adj.get_width(); j++)
-                {
-                    if (prev[i][j] == MUSHROOM && !(adj[i][j] == 2 || adj[i][j] == 3))
-                        current[i][j] = EMPTY;
-                    if (prev[i][j] == EMPTY && adj[i][j] == 3)
-                        current[i][j] = MUSHROOM;
-                }
-            }
-        }
-        return current;
-    }
-
-    Table<int> adjacents(const Table<char> &table) const
-    {
-        Table<int> adj(table.get_height(), table.get_width(), 0);
-
         for (std::size_t i = 0; i < table.get_height(); i++)
         {
             for (std::size_t j = 0; j < table.get_width(); j++)
             {
                 if (table[i][j] == EMPTY)
-                    continue;
-
-                for (int i_diff = -1; i_diff <= 1; i_diff++)
                 {
-                    for (int j_diff = -1; j_diff <= 1; j_diff++)
-                    {
-                        if (i_diff == 0 && j_diff == 0)
-                            continue;
-
-                        std::size_t new_i = (i + i_diff + table.get_height()) % table.get_height();
-                        std::size_t new_j = (j + j_diff + table.get_width()) % table.get_width();
-                        adj[new_i][new_j]++;
-                    }
+                    table[i][j] = MUSHROOM;
+                    return;
                 }
+                table[i][j] = EMPTY;
             }
         }
+    }
 
-        return adj;
+    Table<char> update(const Table<char> &table) const
+    {
+        Table<char> result = table;
+
+        for (std::size_t i = 0; i < table.get_height(); i++)
+        {
+            for (std::size_t j = 0; j < table.get_width(); j++)
+            {
+                int adj = mushrooms(table, i, j);
+                if (table[i][j] == MUSHROOM && !(adj == 2 || adj == 3))
+                    result[i][j] = EMPTY;
+                if (table[i][j] == EMPTY && adj == 3)
+                    result[i][j] = MUSHROOM;
+            }
+        }
+        return result;
+    }
+
+    Table<char> update(const Table<char> &table, const std::size_t level) const
+    {
+        Table<char> result = table;
+        for (std::size_t i = 0; i < level; i++)
+            result = update(result);
+        return result;
+    }
+
+    int mushrooms(const Table<char> &table, const std::size_t index1, const std::size_t index2) const
+    {
+        int result = 0;
+        for (size_t i = index1 - 1 + table.get_height(); i <= index1 + 1 + table.get_height(); i++)
+        {
+            for (size_t j = index2 - 1 + table.get_width(); j <= index2 + 1 + table.get_width(); j++)
+            {
+                if (table[i % table.get_height()][j % table.get_width()] == MUSHROOM)
+                    result++;
+            }
+        }
+        return table[index1][index2] == MUSHROOM ? --result : result;
     }
 };
 
