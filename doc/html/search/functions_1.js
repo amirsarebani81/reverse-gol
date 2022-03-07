a5690701d4db33b0deb59e5ac589690df54d078c
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-03-07 21:50:12 +0330

    Update docs

--- doc/html/search/functions_1.js
+++ doc/html/search/functions_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['get_5fheight_17',['get_height',['../classTable.html#ad6cda0c0acfa68b6d46d67afb0fc7ca2',1,'Table']]],
   ['get_5fwidth_18',['get_width',['../classTable.html#a3686322461f2f50c4fdc09b9e585e974',1,'Table']]],
-  ['gol_19',['Gol',['../classGol.html#a127b69d1f0c06910a8030b0cebc1e7c2',1,'Gol']]]
+  ['gol_19',['Gol',['../classGol.html#ac440be8efd9b36760e7ca3a595d75423',1,'Gol::Gol()'],['../classGol.html#a127b69d1f0c06910a8030b0cebc1e7c2',1,'Gol::Gol(const Table&lt; char &gt; &amp;table, const std::size_t lev)']]]
 ];
