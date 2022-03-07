4a8560a8ffd170184e34808657282e21e725f716
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-03-07 16:03:13 +0330

    Make << overload friend of Table class

--- table.hpp
+++ table.hpp
@@ -7,7 +7,7 @@
 
 /**
  * @brief A wrapper class for std::vector that can use as a container for tables
- * 
+ *
  * @tparam T The type of the elements
  */
 template <typename T>
@@ -17,17 +17,28 @@ private:
     std::size_t height, width;
     std::vector<T> vec;
 
+    friend std::ostream &operator<<(std::ostream &os, const Table<T> &table)
+    {
+        for (std::size_t i = 0; i < table.get_height(); i++)
+        {
+            for (std::size_t j = 0; j < table.get_width(); j++)
+                os << table.vec[i * table.get_width() + j];
+            os << std::endl;
+        }
+        return os;
+    }
+
 public:
     /**
      * @brief Construct a new Table object
-     * 
+     *
      */
     Table()
         : height(0), width(0), vec(std::vector<T>(0)) {}
 
     /**
      * @brief Construct a new Table object
-     * 
+     *
      * @param h Height of table
      * @param w Width of table
      */
@@ -36,7 +47,7 @@ public:
 
     /**
      * @brief Construct a new Table object
-     * 
+     *
      * @param h Height of table
      * @param w Width of table
      * @param v Initial value of table cells
@@ -46,39 +57,39 @@ public:
 
     /**
      * @brief Get the height object
-     * 
-     * @return std::size_t 
+     *
+     * @return std::size_t
      */
     std::size_t get_height() const { return height; }
 
     /**
      * @brief Get the width object
-     * 
-     * @return std::size_t 
+     *
+     * @return std::size_t
      */
     std::size_t get_width() const { return width; }
 
     /**
      * @brief Check whether container is empty
-     * 
-     * @return true 
-     * @return false 
+     *
+     * @return true
+     * @return false
      */
     bool empty() const { return !get_height() || !get_width(); }
 
     /**
      * @brief [] operator
-     * 
+     *
      * @param index
-     * @return std::vector<T>::iterator 
+     * @return std::vector<T>::iterator
      */
     typename std::vector<T>::iterator operator[](std::size_t index) { return vec.begin() + index * get_width(); }
 
     /**
      * @brief [] operator
-     * 
-     * @param index 
-     * @return std::vector<T>::const_iterator 
+     *
+     * @param index
+     * @return std::vector<T>::const_iterator
      */
     typename std::vector<T>::const_iterator operator[](std::size_t index) const
     {
@@ -87,10 +98,10 @@ public:
 
     /**
      * @brief == operator
-     * 
+     *
      * @param table rvalue
-     * @return true 
-     * @return false 
+     * @return true
+     * @return false
      */
     bool operator==(const Table<T> &table) const
     {
@@ -99,24 +110,12 @@ public:
 
     /**
      * @brief != operator
-     * 
+     *
      * @param table rvalue
-     * @return true 
-     * @return false 
+     * @return true
+     * @return false
      */
     bool operator!=(const Table<T> &table) const { return !(this == table); }
 };
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const Table<T> &table)
-{
-    for (std::size_t i = 0; i < table.get_height(); i++)
-    {
-        for (std::size_t j = 0; j < table.get_width(); j++)
-            os << table[i][j];
-        os << std::endl;
-    }
-    return os;
-}
 
 #endif
